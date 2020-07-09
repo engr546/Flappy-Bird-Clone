a3e35e1b569844e429cd19c52301a6f31c72fd95
engr546 <engrflare@gmail.com>
2020-07-09 21:52:47 +0800

    Animations
    - Added Animations
    - Updated PlayerController.cs

--- Flappy Bird/Assets/Scripts/PlayerController.cs
+++ Flappy Bird/Assets/Scripts/PlayerController.cs
@@ -6,13 +6,15 @@ public class PlayerController : MonoBehaviour
     public float upForce = 200f;
 
     Rigidbody2D rb2d;
-    
+    Animator animator;
+
     bool isDead;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,13 +23,16 @@ public class PlayerController : MonoBehaviour
 
         if (isDead)
             return;
-        
+
         // Jump Player
         if (Input.GetMouseButtonDown(0))
         {
             // Reset the velocity to zero
             rb2d.velocity = Vector2.zero;
             rb2d.AddForce(new Vector2(0, upForce));
+
+            // Set Animation
+            animator.SetTrigger("Flap");
         }
 
     }
@@ -35,6 +40,7 @@ public class PlayerController : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision)
     {
         isDead = true;
+        animator.SetTrigger("Die");
     }
 
 }
